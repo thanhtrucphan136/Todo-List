fef55de638800a6ac9ea1eafe94be59a0a21c024
Truc Phan <trucphan@my.unt.edu>
2022-08-19 00:11:15 -0500

    create task class

---
+++ src/task.js
@@ -0,0 +1,38 @@
+export default class Task{
+    constructor(title, description, dueDate = 'No due date'){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    setTitle(title){
+        this.title = title;
+    }
+
+    getTitle(){
+        return this.title;
+    }
+
+    setDescription(description){
+        this.description = description;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+
+    setDueDate(dueDate){
+        this.dueDate = dueDate;
+    }
+
+    getDueDate(){
+        return this.dueDate;
+    }
+
+    formatDate(){
+        const day = this.dueDate.split('/')[0];
+        const month = this.dueDate.split('/')[1];
+        const year = this.dueDate.split('/')[2];
+        return `${month}/${day}/${year}`;
+    }
+}
