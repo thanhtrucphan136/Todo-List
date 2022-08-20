a0779f7e4f060c3f232a8eae5fe276d9b91d9d95
Truc Phan <trucphan@my.unt.edu>
2022-08-19 23:55:40 -0500

    create add task area

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log('Hello');
+import Task from "./task";
+import Project from "./project";
+import TodoList from "./todoList";
+/*
+const loadProject = (() => {
+    const display = document.querySelector('.display');
+    const projectBtns = document.querySelectorAll('.project-btn');
+    const projectName = document.querySelector('h2');
+    projectBtns.forEach((button) => {
+        button.addEventListener('click', () => {
+            projectName.textContent = button.textContent;
+        });
+    });
+})();*/
+
+const addTaskBtn = document.querySelector('.add-task-btn');
+addTaskBtn.addEventListener('click', addTaskArea);
+function addTaskArea(){
+    const taskName = document.createElement('input');
+    taskName.type = 'text';
+    const addBtn = document.createElement('button');
+    addBtn.textContent = 'Add';
+    const cancelBtn = document.createElement('button');
+    cancelBtn.textContent = 'Cancel';
+    const addTask = document.querySelector('.add-task');
+    addTask.appendChild(taskName);
+    addTask.appendChild(addBtn);
+    addTask.appendChild(cancelBtn);
+}
+
+function createTask(taskName){
+    return new Task(taskName);
+}
